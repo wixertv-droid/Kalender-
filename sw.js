44a2cf6e1fca891ecc8a382c3890c090fb416936
wixertv-droid <wixertv@googlemail.com>
2026-02-25 20:22:02 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,42 @@
-// Ein simpler Service Worker, der die App installierbar macht
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Installiert');
+const CACHE_NAME = 'agenda-2050-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './woche.html',
+  './jahr.html',
+  './kunden.html',
+  './einstellungen.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icon.png'
+];
+
+// Installation: Dateien in den Cache laden
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-    // Lässt alle Anfragen normal durch, reicht für unser Offline-Speicher-System
+// Aktivierung: Alte Caches löschen
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Strategie: Erst im Cache suchen, dann Netzwerk (Offline-First)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });

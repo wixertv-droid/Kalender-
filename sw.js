b1c2773e0e451cc59224c1093c7bbef78eacb5d1
wixertv-droid <wixertv@googlemail.com>
2026-02-25 18:30:35 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-// Ein simpler Service Worker, der die App installierbar macht
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Installiert');
-});
-
-self.addEventListener('fetch', (e) => {
-    // Lässt alle Anfragen normal durch, reicht für unser Offline-Speicher-System
-});
